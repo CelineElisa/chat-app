0adf0d732e1550cd2bfa560d38ca542dc040d0d8
Céline Cottier <cottier.celine@gmail.com>
2020-04-01 19:30:47 +0200

    ContactList created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact.js';
+import ContactList from './components/ContactList.js';
 
 function App() {
   return (
     <div className="App">
-     <Contact name="Mary Pops" avatar="https://randomuser.me/api/portraits/women/83.jpg" online/>
-     <Contact name="Carole Murphy" avatar="https://randomuser.me/api/portraits/women/8.jpg" />
-     <Contact name="Christy Lane" avatar="https://randomuser.me/api/portraits/women/23.jpg" online/>
+     <ContactList/>
     </div>
   );
 }
