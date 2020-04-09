ca12d9ebb98828053c094ea33d15fc619ef4a022
Céline Cottier <cottier.celine@gmail.com>
2020-04-09 16:56:18 +0200

    switch status on click

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,32 @@ import React from 'react';
 import './Contact.css'
 import PropTypes from 'prop-types';
 
-function Contact(props){
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            online:this.props.online
+        };
+    }
+    render(){
     return(
         <div className="Contact">
-            <img className="avatar" src={props.avatar}/>
+            <img className="avatar" src={this.props.avatar}/>
             <div>
-                <h2 className="name">{props.name}</h2>
-                <div className="status">
-                    <div className={props.online ? "status-online":"status-offline"}></div>
-                    <p>{props.online ? "Online":"Offline"}</p>
+                <h2 className="name">{this.props.name}</h2>
+                <div className="status"
+                onClick={event=>{
+                    const isOnline=!this.state.online;
+                    this.setState({online:isOnline});
+                }}
+                >
+                    <div className={this.state.online ? "status-online":"status-offline"}></div>
+                    <p>{this.state.online ? "Online":"Offline"}</p>
                 </div>
             </div>
         </div>
     );
+    }
 }
 
 
